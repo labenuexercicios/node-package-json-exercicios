9d4e0dbb410cfe60b75100f40ad1b9319f1be678
Medusa-85 <lima.mariana012@gmail.com>
2023-01-03 14:18:32 -0300

    Exercícios node.js e packege.json

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Seu aplicativo está funcionando.")
+
+const input = process.argv[2]
+
+console.log(input)
---
+++ par-ou-impar.js
@@ -0,0 +1,5 @@
+const numUsuario = process.argv[3]
+const parImparUsuario = process.argv[2]
+
+
+
