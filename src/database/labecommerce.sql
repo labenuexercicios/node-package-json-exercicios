eecefdfbaba20d1086dd068a237a6f36823cfee8
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2023-01-23 09:16:53 -0300

    mais queries adicionadas

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -31,3 +31,73 @@ VALUES ("p001", "produto1", 100.0, "cat1"),
 PRAGMA table_info ('products'); --mostrar tabela
 
 SELECT * FROM products; --pegar as infos da tabela
+
+-----------------------------------------
+ --GetAllUsers
+SELECT * FROM users;
+
+ --SearchProductByName
+SELECT * FROM products
+WHERE name LIKE "produto2%";
+
+  --CreatePurchase
+
+CREATE TABLE 
+purchases (
+    userId TEXT PRIMARY KEY NOT NULL,
+    productId TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    totalPrice REAL NOT NULL
+);
+INSERT INTO purchases (userId, productId, quantity, totalPrice)
+VALUES ("a001", "p003", 1, 300),
+("a002", "p003", 2, 600);
+
+
+SELECT * FROM purchases;
+
+--SearchProductsById
+SELECT * FROM products 
+WHERE id = "p002";
+
+  --DeleteProductById
+DELETE FROM products
+WHERE id = "p002";
+
+  --DeleteUserById
+DELETE FROM users
+WHERE id = "a002";
+
+--EditUSerById
+UPDATE users
+SET password = "bananinha123"
+WHERE id = "a001";
+
+
+--EditProductById
+UPDATE products
+SET price = 50
+WHERE id = "p001";
+
+
+--EditUsersEmailById
+UPDATE users
+SET email = "teste@gmail.com"
+WHERE id = "a001";
+
+
+--GetUserCrescentOrder
+SELECT * FROM users
+ORDER BY email ASC;
+
+
+--GetProductsByPrice
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 20;
+
+
+--GetProductsByBetweenPrice
+SELECT * FROM products
+WHERE price >=200 AND price <=1000
+ORDER BY price ASC;
